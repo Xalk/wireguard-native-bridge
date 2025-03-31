2c06f23e4a9a5cc3a7c84dd42232bcd121a6d2a5
Artem Khalturin <crm.32526@gmail.com>
2025-03-31 14:23:25 +0300

    chore: initial commit

---
+++ ios/WireguardNativeBridge.swift
@@ -0,0 +1,8 @@
+@objc(WireguardNativeBridge)
+class WireguardNativeBridge: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
